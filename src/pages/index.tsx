8481368383d960a9e5052cf538ea083ef730147a
Dinobergue Viana <dinobergueviana@gmail.com>
2021-10-03 18:21:54 -0300

    creates Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
       <Head>
-          <title>ig.news</title>
-        </Head>
+        <title>ig.news</title>
+      </Head>
       <h1> 
         Hello, World!
       </h1>
