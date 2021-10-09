2d1c0bd682f0ce0d4462e6ac6368716f49d4c9d6
Dinobergue Viana <dinobergueviana@gmail.com>
2021-10-08 22:16:21 -0300

    allows save user email on faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,8 @@
-// import NextAuth from "next-auth"
-// import Provider from "next-auth/providers"
-
-// export default NextAuth({   
-//   providers: [
-//     Provider.GitHub({
-//       clientId: process.env.GITHUB_CLIENT_ID,
-//       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-//       scope: 'read:user'
-//     }),     
-//   ],
-// })
-
+import { query as q } from 'faunadb' 
 import NextAuth from "next-auth"
 import Provider from "next-auth/providers"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({   
   providers: [
@@ -22,4 +12,26 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        );
+
+        return true;
+
+      } catch {
+        return false
+      }
+    }
+  }
 })
